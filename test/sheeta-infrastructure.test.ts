88d8821a93d2e580205e83aa9d37a4cff60afabd
Ian Cullinane <ian.cullinane@gmail.com>
2022-02-23 15:50:33 -0500

    sheeta working infrastructure

---
+++ test/sheeta-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SheetaInfrastructure from '../lib/sheeta-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sheeta-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SheetaInfrastructure.SheetaInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
